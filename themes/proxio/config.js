c634195c7f74374f2c15ffafed16cca8e8d36606
tablexx <z345620622@gmail.com>
2025-04-23 16:16:17 +0800

    Update config.js

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -10,9 +10,9 @@ const CONFIG = {
     PROXIO_HERO_TITLE_1: 'CHG华夏神明降临', // 英雄区文字
     PROXIO_HERO_TITLE_2: '欢迎加入', // 英雄区文字
     // 英雄区两个按钮，如果TEXT留空则隐藏按钮
-    PROXIO_HERO_BUTTON_1_TEXT: '申请加群', // 英雄区按钮
+    PROXIO_HERO_BUTTON_1_TEXT: '成员列表', // 英雄区按钮
     PROXIO_HERO_BUTTON_1_URL:
-        'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
+        'https://r2.zako.cc/group', // 英雄区按钮
     PROXIO_HERO_BUTTON_2_TEXT: '在Github上关注', // 英雄区按钮
     PROXIO_HERO_BUTTON_2_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
     PROXIO_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
